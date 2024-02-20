ba95e107ee53ac831623b68772fbd632a725ea20
Vaibhav <vsachdeva4859@gmail.com>
2024-02-21 04:22:23 +0530

    shop updated with sql

--- shop project with node and sql or file databse/routes/shop.js
+++ shop project with node and sql or file databse/routes/shop.js
@@ -4,11 +4,12 @@ const router = express.Router();
 
 router.get("/", shopController.getIndex);
 router.get("/products", shopController.getProducts);
-router.get("/product/:productId", shopController.getProduct);
+router.get("/products/:productId", shopController.getProduct);
 router.get("/cart", shopController.getCart);
 router.post("/cart", shopController.postCart);
-router.get("/checkout", shopController.getCheckout);
+// router.get("/checkout", shopController.getCheckout);
 router.get("/orders", shopController.getOrders);
 router.post("/cart-delete-item", shopController.postCartDeleteProduct);
+router.post("/create-order", shopController.postOrder);
 
 module.exports = router;
